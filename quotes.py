dc7921795304cfa8e505af7cfe6d87aa8db80242
ayasebun <102267002+ayasebun@users.noreply.github.com>
2022-07-06 19:04:22 -0500

    Issues with quotes

---
+++ quotes.py
@@ -0,0 +1,99 @@
+quotes = [
+"im more of a power bottom",
+"2 PICKLES?",
+"With your willy?",
+"why dont you run into sum bitches",
+"Can I cum",
+"Can I rub two waters together to make fire?",
+"you finally got sum bitches",
+"alexa, give me a juice fart",
+"Oh God I'm not a scaley I swear, I just studied snakes alot in high school for a club I was in",
+"Jesus fucking christ that dude is denser than a black hole",
+"im off to be with the mighty discoid orb in the sky",
+"call me a nature's valley bar, cuz I'm about to crumb",
+"HES ARMED AND GOT A FAT DUMPY",
+"I'm gonna park my dick in your ass in a second",
+"ALSO YOU WANT YOUR BOTS TO FUCK MY BUTT HELLO",
+"Please no I have 2 kids",
+"There a penis in my dms and I'm in public",
+"MAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAN",
+"maaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaan",
+"i didn do nothin",
+"KILL JOHN LENIN",
+"DOOM UGH OF- THE LIVING UGH",
+"i dont want to be the peggie, i never said anything about the pegger",
+"cock in my ass",
+"Yoshi  Peez nutz",
+"tell your car i said hi",
+"You have to use your eyeballs and brain, which is really hard when both are leaking out your head",
+"tell earth dragon i love him",
+"USPS PRIORITY MAIL FLAT RATE BOXES ONLY FROM THE POSTAL SERVICE, A SIMPLER WAY TO SHIP. IF IT FITS, IT SHIPS FOR A LOW FLAT RATE",
+"I add my own cream to my ice cream",
+"I'd let him have my top half",
+"Being a girl and being hung are not mutually exclusive tbf"
+]
+
+questions = [
+"Can I cum",
+"Can I rub two waters together to make fire?",
+"Would you rather fuck a mermaid that's the fish top half or bottom half",
+"BRO WHAT DID I DO",
+"how many fuckin credit card tiddies do you have",
+"Can I be the pegger?",
+"can you make each tentacle into a penis"
+]
+
+
+responses = [
+"Girl that's a bootyhole",
+"Shut up pegging master",
+"HARDER",
+"THICCEN nuggets",
+"OOO What else",
+"I have a cloaca",
+"what did i dooooo",
+"Please no I have 2 kids",
+"MAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAN",
+"maaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaan",
+"i didn do nothin"
+]
+
+bro = [
+"BRO WHAT DID I DO",
+"BRO I DONT WANT ANY MANDINGO DICK",
+"Bro I'm lookin to be aborted right now"
+]
+
+crim = [
+"crims pussy doth poppeth severely",
+"I mean I ain't gonna force ya to do nothin, but that means you can't fill Crims void",
+"Man I was gonna make a joke about me filling crims void, but now that everything is being recorded idk how retarded I should be",
+"help, crim said hes gonna eat my beans",
+"Damn when is daddy crim gonna amongus me",
+"I'd kill myself for crims bussy",
+"so would crims ass be a bussy or a chussy?",
+"will crim shake that dumpy on dancer?"
+]
+
+sasha = [
+"id fuck sasha",
+"is sasha on chaterbate?",
+"can I have sasha's irl tiddies on a billboard?",
+"Sasha lookin to get rammed",
+"I've gotta hide my hot bussy, Sasha is coming from me"
+]
+
+marvin = [
+"Yea imma fuck Marvin",
+"HOW DO YOU EXPECT ME TO RESIST HIS TIGHT LITTLE BOY PUSSY",
+"man why do i always fuck marvin",
+"Marvin you wanna be my magick snail?",
+"does marvin like penis?",
+"whats wrong marvin, dontcha wanna try rubbin our waters together?"
+]
+
+naiah = [
+"id go gay for naiah",
+"why is naiah so mean",
+"did naiah get some bitches",
+]
